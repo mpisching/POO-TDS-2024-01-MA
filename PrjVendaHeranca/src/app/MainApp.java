17a64654417cbc7d03754024176e327de7f3658d
Marcos André Pisching <mpisching@gmail.com>
2024-04-03 18:07:35 -0300

    POO - estudo 3 - Introdução a Heranca

---
+++ PrjVendaHeranca/src/app/MainApp.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import domain.Categoria;
+import domain.ESituacao;
+import domain.Estoque;
+import domain.Fornecedor;
+import domain.Nacional;
+import domain.Produto;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Professor
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+        Fornecedor fornecedor = new Fornecedor(1, "Toshiba", "contato@toshiba.com", "4899938983" );
+        Nacional fornecedorNacional = new Nacional("88888888893943", 2, "Positivo", "contato@positivo.com.br", "23423242");
+        
+        print(fornecedorNacional);
+    }
+    
+    public static void print(Fornecedor fornecedor) {
+        System.out.println("Fornecedor: " + fornecedor);
+    }
+
+}
